e133302c059a3d53603b35d640b0865f8281c524
= <=>
2021-04-07 09:15:03 +0700

    thanhyou00

---
+++ Main.java
@@ -0,0 +1,70 @@
+
+package AsmFinal;
+import java.util.Scanner;
+/**
+ *
+ * @author Nguyen Truc
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ServicePhoneBook book = new ServicePhoneBook();
+        System.out.println("+------------------MENU------------------+");
+        System.out.println("|1. Thêm người vào danh bạ               |");
+        System.out.println("|2. Sửa người trong danh bạ              |");
+        System.out.println("|3. Xóa người trong danh bạ              |");
+        System.out.println("+4. Tìm kiếm theo tên hoặc số điện thoại +");
+        System.out.println("+5. Tải thông tin danh bạ                +");
+        System.out.println("+6. Tìm kiếm theo tên gần đúng           +");
+        System.out.println("+7. Tìm kiếm theo số điện thoại gần đúng +");
+        System.out.println("+8. Sắp xếp danh bạ ngược, xuôi          +");
+        System.out.println("+----------------------------------------+");
+        int choice;
+        System.out.println("Mời bạn chọn chức năng : ");
+        choice = Integer.parseInt(scanner.nextLine());
+        switch(choice) {
+            case 1: {
+                book.AddPerson();
+                book.getAddPerson();
+                break;
+            }
+            case 2 : {
+                book.MadeAvailable();
+                book.EditPerson();
+                book.getEditPerson();
+                break;
+            }
+            case 3 :{
+                book.MadeAvailable();
+                book.DeletePerson();
+                book.getDeletePerson();
+                break;
+            }
+            case 4 : {
+                book.MadeAvailable();
+                book.SearchArr();
+                break;
+            }
+            case 5 : {
+                //Load thông tin
+                break;
+            }
+            case 6 :{
+                book.MadeAvailable();
+                book.SearchAppName();
+                break;
+            }
+            case 7 : {
+                book.MadeAvailable();
+                book.SearchAppNum();
+                break;
+            }
+            case 8: {
+                book.MadeAvailable();
+                book.SortArr();
+                break;
+            }
+            default:System.out.println("Nhập sai vui lòng nhập lại ! "); break;
+        }
+    }
+}
