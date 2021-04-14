cd77e8fcd858812c24cf9a909d54868c5f4b5888
thanhyou00 <74861823+thanhyou00@users.noreply.github.com>
2021-04-14 22:23:31 +0700

    Update ServicePhoneBook.java

--- ServicePhoneBook.java
+++ ServicePhoneBook.java
@@ -1,20 +1,18 @@
 
 package AsmFinal;
-
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
-
 /**
  *
  * @author Nguyen Truc
  */
-
 public class ServicePhoneBook {
     ArrayList<PhoneBook> _lstphone = new ArrayList<>();
     PhoneBook _pb ;
     Scanner _sc = new Scanner(System.in);
-    // Tạo liên hệ có sẵn
-    void MadeAvailable() {
+    void MadeAvailable() {     // Tạo liên hệ có sẵn
         PhoneBook pb1 = new PhoneBook("0973919394","email1","note1","Nguyen","Van","Trung",2001,"Nam");
         pb1.inraManhinhPhone();
         _lstphone.add(pb1);
@@ -61,8 +59,7 @@ public class ServicePhoneBook {
         pb15.inraManhinhPhone();
         _lstphone.add(pb15);
     }
-    // Thêm liên hệ mới
-    void AddPerson() {
+    void AddPerson() {    // Thêm liên hệ mới
         int size;
         System.out.println("Bạn muốn thêm bao nhiêu người : ");
         size = Integer.parseInt(_sc.nextLine());
@@ -87,20 +84,18 @@ public class ServicePhoneBook {
         System.out.println("Nhập vào ghi chú : ");
         _pb.setNote(_sc.nextLine());
         _lstphone.add(_pb);// Nhập xong từng đối tượng và thêm vào danh sách
-        }  
-    }
+        }  }
     void getAddPerson() {
         for (PhoneBook x : _lstphone) {
             x.inraManhinhPhone();
-        }
-    }
-    // Sửa liên hệ
-    void EditPerson() {
+        } }
+        public void EditPerson() {   // Sửa liên hệ
         System.out.println("Nhập số điện thoại người liên hệ cần sửa : ");
         String phoneNum = _sc.nextLine();
         for (int i = 0; i < _lstphone.size(); i++) {
         if(phoneNum.equals(_lstphone.get(i).getNumberPhone())){
         int choice;
+        do {
             System.out.println("1. Sửa họ");
             System.out.println("2. Sửa tên đệm");
             System.out.println("3. Sửa tên");
@@ -109,154 +104,165 @@ public class ServicePhoneBook {
             System.out.println("6. Sửa số điện thoại");
             System.out.println("7. Sửa email");
             System.out.println("8. Sửa ghi chú");
+            System.out.println("9. Thoát");
             System.out.println("Vui lòng nhập lựa chọn : ");
             choice = _sc.nextInt();
         switch(choice) {
             case 1 : 
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào họ : ");
+        System.out.println("Nhập vào họ mới: ");
        String ho = _sc.nextLine();
         _lstphone.get(i).setHo(ho);
+        _lstphone.add(_pb);
+        getEditPerson();
         break;
             case 2 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào tên đệm : ");
+        System.out.println("Nhập vào tên đệm mới: ");
         String tenDem = _sc.nextLine();
         _lstphone.get(i).setTenDem(tenDem);
+        getEditPerson();
         break;
             case 3 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào tên : ");
+        System.out.println("Nhập vào tên mới: ");
         String name = _sc.nextLine();
         _lstphone.get(i).setTen(name);
+        getEditPerson();
         break;
             case 4 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào năm sinh : ");
+        System.out.println("Nhập vào năm sinh mới: ");
         int namSinh = Integer.parseInt(_sc.nextLine());
         _lstphone.get(i).setNamSinh(namSinh);
+        getEditPerson();
         break;
             case 5 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào giới tính : ");
+        System.out.println("Nhập vào giới tính mới: ");
         String gioiTinh = _sc.nextLine();
-        _lstphone.get(i).setGioiTinh(gioiTinh);
+        _lstphone.get(i).setGioiTinh(gioiTinh); 
+        getEditPerson();
         break;
             case 6 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào số điện thoại : ");
+        System.out.println("Nhập vào số điện thoại mới: ");
         String numberPhone = _sc.nextLine();
         _lstphone.get(i).setNumberPhone(numberPhone);
+        getEditPerson();
         break;
             case 7 :
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào email : ");
+        System.out.println("Nhập vào email mới: ");
         String email = _sc.nextLine();
         _lstphone.get(i).setEmail(email);
+        getEditPerson();
         break;
             case 8:
         _sc = new Scanner(System.in);        
-        System.out.println("Nhập vào ghi chú : ");
+        System.out.println("Nhập vào ghi chú mới : ");
         String note = _sc.nextLine();
         _lstphone.get(i).setNote(note);
+        getEditPerson();
         break;
-        }
+            case 9: {
+                System.exit(0); break;
             }
-        }
-    }
+        } }  while(!(choice == 9)) ; } } }
     void getEditPerson() {
         for (PhoneBook x : _lstphone) {
             x.inraManhinhPhone();
-        }
-    }
-    // Xóa liên hệ
-    void DeletePerson() {
+        } }
+    void DeletePerson() {    // Xóa liên hệ
         _sc = new Scanner(System.in);
         System.out.println("Nhập số điện thoại liên hệ người cần xóa : "); 
         String phoneNum = _sc.nextLine();
         for (int i = 0; i <_lstphone.size(); i++) {
             if(phoneNum.equals(_lstphone.get(i).numberPhone)){
                 _lstphone.remove(i);
-            }
-        }
-    }
+            } } }
     void getDeletePerson() {
         for (PhoneBook y : _lstphone) {
             y.inraManhinhPhone();
-        }
-    }
-    // Tìm kiếm theo tên
-    void SearchName() {
-        String namePer;
-        int i;
-        do {
+        } }
+   public  void SearchName() {// Tìm kiếm theo tên
         System.out.println("Nhập tên người cần tìm : ");
-        namePer = _sc.nextLine();
-        for ( i = 0; i < _lstphone.size(); i++) {
-        if(namePer.equals(_lstphone.get(i).getTen())){
+        String namePer = _sc.nextLine();
+        for (int i = 0; i < _lstphone.size(); i++) {
+        if(namePer.equals(_lstphone.get(i).getTen().toLowerCase())){
         System.out.println("Họ : "+_lstphone.get(i).getHo()+", tên đệm : "+
         _lstphone.get(i).getTenDem()+", tên : "+_lstphone.get(i).getTen()+""
         + ", năm sinh : "+_lstphone.get(i).getNamSinh()+", giới tính : "+_lstphone.get(i).getGioiTinh()+
         " Số điện thoại : "+_lstphone.get(i).numberPhone+ " ,Email : "+_lstphone.get(i).email+" ,Ghi chú : "+_lstphone.get(i).note);
+        } } }
+   // Load
+           void LoaddingBook() {
+        int mydate=2000,ns;
+        String ten,gioiTinh,ho,tenDem,sodienThoai,email;
+        do {
+            System.out.println("Mời bạn nhập họ: ");
+            ho = _sc.nextLine();
+            System.out.println("Mời bạn nhập tên đệm:");
+            tenDem = _sc.nextLine();
+            System.out.println("Mời bạn nhập tên :");
+            ten = _sc.nextLine();
+            System.out.println("Mời bạn nhập số điện thoại:");
+            sodienThoai = _sc.nextLine();
+            System.out.println("Mời bạn nhập email:");
+            email = _sc.nextLine();
+            System.out.println("Mời bạn nhập giới tính:");
+            gioiTinh = _sc.nextLine();
+            System.out.println("Mời bạn nhập năm sinh:");
+            ns = Integer.parseInt(_sc.nextLine());
+            String resultNam = (gioiTinh.equalsIgnoreCase("nam")&&(mydate> ns))?"Anh":null;
+            String resultNu = (gioiTinh.equalsIgnoreCase("nữ")&&(mydate> ns))?"Chị":null;
+            System.out.printf("Chao %s %s %s %s,số điện thoại:%s , email: %s \n",
+                    (resultNam!=null)?resultNam:(resultNu!=null)?resultNu:
+                    (resultNam==null&&resultNu==null&&mydate>ns)?"Em":"Bạn",ho,tenDem,ten,sodienThoai,email );
+        } while(true);       
         }
-        }
-        } while(true);
-    }
-    //Tìm kiếm theo số điện thoại
-    void SearchNumber() {
-        do {            
-            System.out.println("Nhập số điện thoại người cần tìm :");
-            String phoneNumber = _sc.nextLine();
-            for (int i = 0; i < _lstphone.size(); i++) {
-                if (phoneNumber.equals(_lstphone.get(i).getNumberPhone())) {
-                    System.out.println("Họ : " + _lstphone.get(i).getHo() + ", tên đệm : "
-                            + _lstphone.get(i).getTenDem() + ", tên : " + _lstphone.get(i).getTen() + ""
-                            + ", năm sinh : " + _lstphone.get(i).getNamSinh() + ", giới tính : " + _lstphone.get(i).getGioiTinh()
-                            + " Số điện thoại : " + _lstphone.get(i).numberPhone + " ,Email : " + _lstphone.get(i).email + " ,Ghi chú : " + _lstphone.get(i).note);                    
-                }
-            }
-        } while (true);
-    }
-    // Tìm kiếm
-    void SearchArr(){
+   
+   //Load
+   public void SearchNumber() {      //Tìm kiếm theo số điện thoại        
+        System.out.println("Nhập số điện thoại người cần tìm :");
+        String phoneNumber = _sc.nextLine();
+        for (int i = 0; i < _lstphone.size(); i++) {
+        if (phoneNumber.equals(_lstphone.get(i).getNumberPhone())) {
+        System.out.println("Họ : " + _lstphone.get(i).getHo() + ", tên đệm : "
+        + _lstphone.get(i).getTenDem() + ", tên : " + _lstphone.get(i).getTen() + ""
+        + ", năm sinh : " + _lstphone.get(i).getNamSinh() + ", giới tính : " + _lstphone.get(i).getGioiTinh()
+        + " Số điện thoại : " + _lstphone.get(i).numberPhone + " ,Email : " + _lstphone.get(i).email + " ,Ghi chú : " + _lstphone.get(i).note);                    
+        }  } }
+    void SearchArr(){// Tìm kiếm
         _sc = new Scanner(System.in);
-        int choice;
+        String choice;
         System.out.println("------------------------------");
         System.out.println("1. Tìm kiếm theo tên  ");
         System.out.println("2. Tìm kiếm theo số điện thoại");
         System.out.println("Bạn chọn ? ");
-        choice = Integer.parseInt(_sc.nextLine());
+        choice = _sc.nextLine();
         switch(choice) {
-            
-            case 1 : {
+            case "1" : {
                 SearchName(); 
                 break;
             }
-            case 2 : {
+            case "2" : {
                 SearchNumber();
                 break;
             }
             default:System.out.println("Nhập sai vui lòng nhập lại !"); break;
-        }
-    }
+        } }
         void SearchAppName() {
-        String namePer;
-        int i;
-        do {
         System.out.println("Nhập tên người cần tìm : ");
-        namePer = _sc.nextLine();
-        for ( i = 0; i < _lstphone.size(); i++) {
-        if(_lstphone.get(i).getTen().startsWith(namePer)){
+        String namePer = _sc.nextLine();
+        for (int i = 0; i < _lstphone.size(); i++) {
+        if(_lstphone.get(i).getTen().toLowerCase().startsWith(namePer)){
         System.out.println("Họ : "+_lstphone.get(i).getHo()+", tên đệm : "+
         _lstphone.get(i).getTenDem()+", tên : "+_lstphone.get(i).getTen()+""
         + ", năm sinh : "+_lstphone.get(i).getNamSinh()+", giới tính : "+_lstphone.get(i).getGioiTinh()+
         " Số điện thoại : "+_lstphone.get(i).numberPhone+ " ,Email : "+_lstphone.get(i).email+" ,Ghi chú : "+_lstphone.get(i).note);
-        }
-        }
-        } while(true);
-    }
-        void SearchAppNum() {
-        do {            
+        } } }
+        void SearchAppNum() {         
         System.out.println("Nhập số điện thoại người cần tìm :");
         String phoneNumber = _sc.nextLine();
         for (int i = 0; i < _lstphone.size(); i++) {
@@ -265,19 +271,8 @@ public class ServicePhoneBook {
         + _lstphone.get(i).getTenDem() + ", tên : " + _lstphone.get(i).getTen() + ""
         + ", năm sinh : " + _lstphone.get(i).getNamSinh() + ", giới tính : " + _lstphone.get(i).getGioiTinh()
         + " Số điện thoại : " + _lstphone.get(i).numberPhone + " ,Email : " + _lstphone.get(i).email + " ,Ghi chú : " + _lstphone.get(i).note);                    
-        }
-        }
-        } while (true);    
-        }
-        // Sắp xếp
-        void Sortreverse() {
-            do {                
-                
-            } while (true);
-        }
-        void SortDown() {
-            
-        }
+        } } }
+        // Sắp xếp ngược, xuôi
         void SortArr() {
             _sc = new Scanner(System.in);
             int choice;
@@ -288,15 +283,28 @@ public class ServicePhoneBook {
             choice = Integer.parseInt(_sc.nextLine());
             switch(choice) {
                 case 1: {
-                    Sortreverse();
+                    Collections.sort(_lstphone, new Comparator<PhoneBook>() {
+                    @Override
+                    public int compare(PhoneBook o1, PhoneBook o2) {
+                    return o1.getNamSinh()-o2.getNamSinh(); 
+                    } }) ;
+                    for (PhoneBook o : _lstphone) {
+                        o.inraManhinhPhone();
+                    }
                     break;
                 }
                 case 2 : {
-                    SortDown();
+                    Collections.sort(_lstphone, new Comparator<PhoneBook>() {
+                    @Override
+                    public int compare(PhoneBook o1, PhoneBook o2) {
+                    return o2.getNamSinh()-o1.getNamSinh(); 
+                    } }) ; 
+                    for (PhoneBook o : _lstphone) {
+                        o.inraManhinhPhone();
+                    }
                     break;
                 }
                 default:System.out.println("Nhập sai vui lòng nhập lại !"); break;
-            }
-        }
-    
+            } }
+
 }
